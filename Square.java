1a3455ad828ab39b2f62a6c0a37512c016472f2c
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2023-03-22 17:24:48 +0000

    Square of number ( without Function )

---
+++ Square.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class square{
+    public static void main(String args[]){
+        int a;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        a=sc.nextInt();
+        System.out.println("Square of the number is: "+(a*a));
+    }
+}

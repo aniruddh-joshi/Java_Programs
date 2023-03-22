3b36d07831a2852bf4464fd1137ad8d891ee5572
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2023-03-22 22:24:35 +0530

    Addn by using Function - Simpler way

---
+++ Addition Type-1.java
@@ -0,0 +1,10 @@
+class demo{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        add(a,b);
+    }
+    public static void add(int x, int y){
+        System.out.println(x + y);
+    }
+}
